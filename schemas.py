b9e046194788ea17f36438e4451aef3e0fdb0ac3
Kitemaggwa Shafic <kitemaggwashafic@gmail.com>
2026-05-29 16:17:38 +0400

    creatted 3 schemas in schema file for base, create and postresponse for return data

--- schemas.py
+++ schemas.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel, ConfigDict, Field
 
 
+# class showing how our schema will look like, this is the base schema that we will use to create our post and also to return our post, we will inherit from this base schema to create our post and also to return our post
 class BasePost(BaseModel):
     title:str = Field(min_length=5, max_length=100)
     content:str =Field(min_length=1)
@@ -12,6 +13,7 @@ class CreatePost(BasePost):
     pass
     
 
+# For returning our data
 class PostResponse(BasePost):
     model_config = ConfigDict(from_attributes=True)
     id:int
